01a56f29055223484781e88df13db22a8d6be5fd
SandaruwanB <sandarusbandara110@gmail.com>
2023-09-11 08:40:19 +0530

    INT app

---
+++ main.js
@@ -0,0 +1,18 @@
+const Electron = require('electron');
+const path = require('path');
+
+const {app, BrowserWindow} = Electron;
+
+const mainWindow = async ()=>{
+    const window = new BrowserWindow(
+        {
+            width : 600,
+            height : 600,
+        }
+    )
+    await window.loadFile(path.join(__dirname, 'app/index.html'));
+}
+
+app.whenReady().then(()=>{
+    mainWindow();
+});
